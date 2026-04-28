47bbdd2e3e182337c0c3bcf45734e0a22e80708b
Pawan2 <p07891256@gmail.com>
2026-04-28 15:00:31 -0700

    initial commit

--- venv/app.py
+++ venv/app.py
@@ -62,6 +62,4 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-import os 
-port = int(os.environ.get("PORT",8080))
-app.run(host="0.0.0.0",port=port, debug=false)
+    app.run(host="0.0.0.0", port=port)
